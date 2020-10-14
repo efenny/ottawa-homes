b990f39e268e0c35086acf6e02b297cfd78dfd2e
efenny <e.freundorfer@gmail.com>
2020-10-14 13:57:59 -0400

    fixed the CTA section and added the card block component

---
+++ components/card_block.php
@@ -0,0 +1,24 @@
+<section id="card_block-<?php echo $currItem; ?>" class="light-blue-bg section-padding-top section-padding-bottom">
+    <?php if(get_sub_field('text')) {?>
+    <div class="row">
+        <div class="col-12 text-center text-blue mb-5"><?php echo get_sub_field('text'); ?></div>
+    </div>
+    <?php } ?>
+    <div class="row justify-content-around w-100">
+        <?php 
+            if( have_rows('cards') ):
+            while( have_rows('cards') ) : the_row(); ?>
+
+        <div class="col-12 col-md-3 text-center">
+            <figure class="mb-4">
+                <img src="<?php echo get_sub_field('icon')['url']; ?>" alt="column icon">
+            </figure>
+            <div class="col-text">
+                <?php echo get_sub_field('text'); ?>
+            </div>
+        </div>
+
+        <?php endwhile;
+            endif; ?>
+    </div>
+</section>
--- components/cta_block.php
+++ components/cta_block.php
@@ -1,17 +1,15 @@
 <?php
-$ctaTitle = !empty(get_sub_field('title')) ? get_sub_field('title') : '';
 $ctaText = !empty(get_sub_field('text')) ? get_sub_field('text') : '';
 $ctaButton = !empty(get_sub_field('button')) ? get_sub_field('button') : array('url' => '', 'title' => '', 'target' => '');
 
 ?>
 
-<section id="cta_block<?php echo $currItem; ?>"
-    class="med-blue-bg position-relative <?php echo get_sub_field('image') ? 'bg-image' : ''; ?>"
+<section id="cta_block-<?php echo $currItem; ?>"
+    class="med-blue-bg position-relative section-padding-top section-padding-bottom <?php echo get_sub_field('image') ? 'bg-image' : ''; ?>"
     style="<?php echo get_sub_field('image') ? 'background-image: url('.get_sub_field('image')['url'].');' : ''; ?>">
     <div class="container">
         <div class="cta-text text-white text-center">
-            <h3><?php echo $ctaTitle; ?></h3>
-            <div class=""><?php echo $ctaText; ?></div>
+            <div class="mb-5"><?php echo $ctaText; ?></div>
             <?php if( have_rows('buttons') ):  ?>
             <div class="button-wrapper">
                 <?php while( have_rows('buttons') ) : the_row();
