07c82bda4ef3fb66adbf3a4af9bb4f783375717d
efenny <e.freundorfer@gmail.com>
2020-10-14 16:02:49 -0400

    fixed contact block markdown

--- components/contact_block.php
+++ components/contact_block.php
@@ -1,11 +1,12 @@
-<section id="contact_block-<?php echo $currItem; ?>" class="section-col-text-img position-relative ">
+<section id="contact_block-<?php echo $currItem; ?>"
+    class="section-padding-top section-padding-bottom light-blue-bg position-relative ">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-6">
-                <?php echo get_sub_field('text'); ?>
+            <div class="col-12 col-md-7">
+                <?php echo get_sub_field('text') ? get_sub_field('text') : ''; ?>
             </div>
-            <div class="col-12 col-md-6">
-                <?php echo do_shortcode(get_sub_field('contact_shortcode')); ?>
+            <div class="col-12 col-md-5">
+                <?php echo get_sub_field('form_shortcode') ? do_shortcode(get_sub_field('form_shortcode')) : 'Please add a form shortcode'; ?>
             </div>
         </div>
     </div>
