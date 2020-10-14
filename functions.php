71a0cb11f5198b1007cc98e2db51933f024e7f08
efenny <e.freundorfer@gmail.com>
2020-10-14 15:18:32 -0400

    footer and it's mark up

--- functions.php
+++ functions.php
@@ -74,6 +74,14 @@ if( function_exists('acf_add_options_page') ) {
             'redirect'      => false,
         ));
     }
+
+    acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
 }
 
 
